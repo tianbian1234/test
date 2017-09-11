52ec3e8811e550ef76ab561b44498e0fb42a6068
gaoyantao <gaoyantao@mrocker.com>
2017-09-11 19:16:43 +0800

    测试页面

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
+import { HashRouter, hashHistory } from 'react-router-dom';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<HashRouter history={hashHistory}>
+  <App>
+
+  </App>
+</HashRouter>, document.getElementById('app'));
